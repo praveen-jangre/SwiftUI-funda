80acfc2de90fd5396cd99da3131dbc038713822b
Praveen Bharat Jangre <praveen.jangre@impelsys.com>
2025-06-04 20:58:05 +0530

    Initial project setup

---
+++ NightWatch/NightWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  NightWatchApp.swift
+//  NightWatch
+//
+//  Created by Praveen Jangre on 30/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct NightWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NightWatchTests/NightWatchTests.swift
@@ -0,0 +1,17 @@
+//
+//  NightWatchTests.swift
+//  NightWatchTests
+//
+//  Created by Praveen Jangre on 30/05/2025.
+//
+
+import Testing
+@testable import NightWatch
+
+struct NightWatchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
